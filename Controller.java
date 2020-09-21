f022c68519ded564fd1ea781f9cf24815e4db6d6
Yevve <70255780+Yevve@users.noreply.github.com>
2020-09-21 16:39:22 +0200

    view added

--- Controller.java
+++ Controller.java
@@ -1,3 +1,16 @@
 public class Controller {
     
+public View _view;
+public Controller(){
+    this._view=new View(this);
+    _view.setVisible(true);
+}    
+
+public void encryptButtonClicked(){
+
+}
+public void decryptButtonClicked(){
+
+}
+
 }
--- EncryptionEngine.java
+++ EncryptionEngine.java
@@ -1,3 +1,23 @@
 public class EncryptionEngine {
-    
+
+    public void setEncryptionKey(){
+
+    }
+
+    public int getEncryptionKey(){
+        
+    return 0;
+    }
+
+    public String encrypt(){
+        
+    return null;
+    }
+
+    public String decrypt(){
+        
+    return null;
+    }
+
+
 }
