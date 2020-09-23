6c6da68057d76f0b519da881b4e6f05e13a2ac6a
Yevve <70255780+Yevve@users.noreply.github.com>
2020-09-23 12:35:12 +0200

    try catch added

--- Controller.java
+++ Controller.java
@@ -8,18 +8,60 @@ public Controller(){
 }    
 
 public void encryptButtonClicked(){
+    
+    
+   try { 
     String key= view.getEncryptionKey();
+    char[] keyCha=key.toCharArray();
+    for(int i=0;i<key.length();i+=1){
+        if(Character.isAlphabetic(keyCha[i])){
+            throw new Exception("Not a numerical key");
+        }
+    }
     String inputText=view.getInputText();
+    char[] cha=inputText.toCharArray();
+    for(int i=0;i<cha.length;i+=1){
+        if(!Character.isUpperCase(cha[i])){
+            throw new Exception("Not a uppercase word!");
+        }
+    }
     encryptionEngine.setEncryptionKey(Integer.parseInt(key));
-    String outputText = encryptionEngine.encrypt(inputText);
+    String outputText = encryptionEngine.encrypt(inputText);   
     view.setOutputText(outputText);
+} catch (Exception e) {
+    String errMsg=e.getMessage();
+    view.setOutputText(errMsg);
+   }
+    
+    
+
 }
 public void decryptButtonClicked(){
-    String key= view.getEncryptionKey();
-    String inputText=view.getInputText();
-    encryptionEngine.setEncryptionKey(Integer.parseInt(key));
-    String outputText = encryptionEngine.decrypt(inputText);
-    view.setOutputText(outputText);
+   
+    try { 
+        String key= view.getEncryptionKey();
+        char[] keyCha=key.toCharArray();
+        for(int i=0;i<key.length();i+=1){
+            if(Character.isAlphabetic(keyCha[i])){
+                throw new Exception("Not a numerical key");
+            }
+        }
+        String inputText=view.getInputText();
+        char[] cha=inputText.toCharArray();
+        for(int i=0;i<cha.length;i+=1){
+            if(!Character.isUpperCase(cha[i])){
+                throw new Exception("Not a uppercase word!");
+            }
+        }
+        encryptionEngine.setEncryptionKey(Integer.parseInt(key));
+        String outputText = encryptionEngine.decrypt(inputText);   
+        view.setOutputText(outputText);
+    } catch (Exception e) {
+        String errMsg=e.getMessage();
+        view.setOutputText(errMsg);
+       }
+        
+        
 }
 
 }
